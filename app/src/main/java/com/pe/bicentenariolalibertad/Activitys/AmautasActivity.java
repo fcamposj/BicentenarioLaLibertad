b7ef1e8b68cc7d2e342a0bfb3869eed4fbf51b98
Falclow <73864088+Falclow@users.noreply.github.com>
2020-11-06 22:24:15 -0500

    CargarJuego

--- app/src/main/java/com/pe/bicentenariolalibertad/Activitys/AmautasActivity.java
+++ app/src/main/java/com/pe/bicentenariolalibertad/Activitys/AmautasActivity.java
@@ -2,7 +2,12 @@ package com.pe.bicentenariolalibertad.Activitys;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,19 +25,32 @@ public class AmautasActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(android.R.attr.windowFullscreen, android.R.attr.windowFullscreen);
         setContentView(R.layout.activity_option_slide);
 
-        Handler handler= new Handler();
-        handler.postDelayed(new Runnable() {
+       //Animation
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView gameTextView = findViewById(R.id.gameTextView);
+        TextView cargandoTextView = findViewById(R.id.cargandoTextView);
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+
+        gameTextView.setAnimation(animacion2);
+        cargandoTextView.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
                 Intent intent = new Intent(AmautasActivity.this, TestActivity.class);
                 startActivity(intent);
                 finish();
-
             }
-        }, 5000);
+        }, 4000);
+
+
+
 
     }
 }
