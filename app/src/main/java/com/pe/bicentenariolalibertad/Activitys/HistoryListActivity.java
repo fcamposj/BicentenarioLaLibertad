7d16609dbe744ded41813935a8e0ad19dd26d36b
Franklin Campos <frank13cmp@gmail.com>
2020-11-08 21:43:20 -0500

    Create HistoryListActivity
    Added HistoryListActivity

---
+++ app/src/main/java/com/pe/bicentenariolalibertad/Activitys/HistoryListActivity.java
@@ -0,0 +1,18 @@
+package com.pe.bicentenariolalibertad.Activitys;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.pe.bicentenariolalibertad.R;
+
+public class HistoryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_list);
+    }
+
+
+}
